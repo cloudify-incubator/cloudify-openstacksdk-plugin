0eedb9af7bd6106fca0734cc239c8899bc52d36c
Mohammed AbuAisha <mohammeda@cloudify.co>
2018-11-01 11:09:27 +0200

    update plugin

--- openstack_sdk/resources/networks.py
+++ openstack_sdk/resources/networks.py
@@ -13,7 +13,9 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-# Based on this documentation: https://docs.openstack.org/openstacksdk/latest/user/proxies/network.html.
+
+# Based on this documentation:
+# https://docs.openstack.org/openstacksdk/latest/user/proxies/network.html.
 
 from openstack_sdk.common import OpenstackResource
 
